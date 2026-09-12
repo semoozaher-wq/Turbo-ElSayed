dfb2bc7d4e85c0a1e582de22619b0c9d44311fac
semoozaher-wq <semoo.zaher@gmail.com>
2026-09-12 20:14:16 +0300

    config

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,29 @@
-"""MoneyPrinterTurbo 应用包元数据。"""
+import sys
 
-__version__ = "1.3.6"
+from app.config import config
+from app.utils.logging_utils import configure_terminal_logger
+
+
+def __init_logger():
+    # _log_file = utils.storage_dir("logs/server.log")
+    _lvl = config.log_level
+
+    configure_terminal_logger(
+        sys.stdout,
+        level=_lvl,
+        colorize=True,
+    )
+
+    # logger.add(
+    #     _log_file,
+    #     level=_lvl,
+    #     format=format_log_record,
+    #     rotation="00:00",
+    #     retention="3 days",
+    #     backtrace=True,
+    #     diagnose=True,
+    #     enqueue=True,
+    # )
+
+
+__init_logger()
