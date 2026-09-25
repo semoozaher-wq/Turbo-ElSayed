da48f7ec542707c296c37b7f7a8e75b6e60e6fb0
semoozaher-wq <semoo.zaher@gmail.com>
2026-09-26 00:15:40 +0300

    Add files via upload

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,20 +1,3 @@
-"""Configuration package public API."""
+"""MoneyPrinterTurbo 应用包元数据。"""
 
-import sys
-
-from app.config import config as config
-from app.utils.logging_utils import configure_terminal_logger
-
-
-def _init_logger() -> None:
-    """Configure the process logger once when the config package is imported."""
-    configure_terminal_logger(
-        sys.stdout,
-        level=getattr(config, "log_level", "INFO"),
-        colorize=False,
-    )
-
-
-_init_logger()
-
-__all__ = ["config"]
+__version__ = "1.3.6"
